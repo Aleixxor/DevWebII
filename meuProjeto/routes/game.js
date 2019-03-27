f1538944bcc554be9598c35c9eba1f845a04f7fb
Aleixxor <alexlouzada2009@gmail.com>
2019-03-27 20:24:18 -0300

    Add files via upload

---
+++ meuProjeto/routes/game.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET game page. */
+
+router.get('/', function(req, res, next) {
+  res.render('game', { title: 'Game' });
+});
+
+module.exports = router;
